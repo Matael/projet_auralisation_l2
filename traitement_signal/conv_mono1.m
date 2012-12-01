6b58c560ab2facaef37c31989487ec784c0888d2
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-12-01 15:14:15 +0100

    Utilisation du script CTTM + recoupage son anécho

--- traitement_signal/conv_mono1.m
+++ traitement_signal/conv_mono1.m
@@ -9,16 +9,19 @@ Ntfd = 2^14;
 Fe = 44100;
 
 start = 273250;
-stop = 378620;
+stop = 341334;
 
-ri1 = load([dossier 'seance2_reverb1/' fichier]);
+target_start = 278700;
+target_stop = 401000;
+
+ri1 = CTTM_textread([dossier 'seance2_reverb1/' fichier],3);
 ri1 = ri1(:,2);
 ri1 = ri1 - mean(ri1);
 ri1 = ri1(start:stop,:);
 ri1 = ri1*.75;
 
 target_sound = wavread('handclaps_44k1.wav');
-target_sound = target_sound*.75;
+target_sound = target_sound(target_start:target_stop)*.75;
 
 % convolution
 result = conv(ri1,target_sound);
