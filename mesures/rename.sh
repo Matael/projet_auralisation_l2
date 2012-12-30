2c3241b601655749b2502603e691d9d8f2af733c
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2012-12-30 20:46:08 +0100

    prise en compte des fichiers Voie 3

--- mesures/rename.sh
+++ mesures/rename.sh
@@ -5,6 +5,7 @@ do
 	if [[ -d $i ]]; then
 		mv $i/Donn{ées\ ,ees_}temporelles_Voie1.wav
 		mv $i/Donn{ées\ ,ees_}temporelles_Voie2.wav
+		mv $i/Donn{ées\ ,ees_}temporelles_Voie3.wav
 		mv $i/Donn{ées\ ,ees_}temporelles.txt
 		mv $i/Donn{ées\ ,ees_}temporelles.uff58
 		mv $i/Configuration{\ ,_}Mesure.txt
